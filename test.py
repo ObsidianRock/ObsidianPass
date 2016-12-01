63335a7ede422e7d65213c3bc74ec09459cfeaa0
Obsidian <ObsidianRock@users.noreply.github.com>
2016-12-01 17:13:33 +0000

    adding exceptions to data input

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ from main import dropx, encrypt, decrypt, sync_push, sync_pull, update_password
 dby = TinyDB('test.json')
 file_name = 'test'
 
+
 def random_char(y):
     return ''.join(random.choice(string.ascii_letters) for _ in range(y))
 
@@ -23,9 +24,10 @@ def test_encrypt_decrypt():
 def test_update_password():
     site = 'LwAhR'
     master = 1234
-    new_password = 8888
+    new_password = random.randint(1, 10000)
     update_password(master, site, new_password, db=dby)
-    assert decrypt(master, site, db=dby) == str(8888)
+    assert decrypt(master, site, db=dby) == str(new_password)
+
 
 
 
